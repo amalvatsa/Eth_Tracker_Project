c4faa23f540bbcbc9d781e9cc9942b1c19ff2f50
Amal Vatsa <86299039+amalvatsa@users.noreply.github.com>
2024-09-10 22:21:01 +0530

    Delete eth_tracker_project directory

--- eth_tracker_project/tracker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Define home route for root URL
-    path('run-tracker/', views.run_tracker, name='run_tracker'),  # Your existing tracker route
-]
